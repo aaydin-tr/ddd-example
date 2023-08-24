bda96bba67e7dc6771c5520178f9fea6050319e5
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-08-24 17:44:40 +0300

    feat: add campaign entity behaviors

--- entity/campaign.go
+++ entity/campaign.go
@@ -1,8 +1,6 @@
 package entity
 
 import (
-	"time"
-
 	"github.com/aaydin-tr/e-commerce/valueobject"
 	"github.com/google/uuid"
 )
@@ -14,8 +12,63 @@ type Campaign struct {
 	Duration               valueobject.Duration
 	PriceManipulationLimit valueobject.PriceManipulationLimit
 	TargetSalesCount       valueobject.TargetSalesCount
-	StartTime              time.Time
-	TotalSales             int
 	Status                 valueobject.Status
-	// AverageItemPrice       valueobject.AverageItemPrice
+	TotalSales             valueobject.Quantity
+	AverageItemPrice       valueobject.Price
+}
+
+func (c *Campaign) IncreaseTotalSales(amount int) error {
+	newTotalSales, err := valueobject.NewQuantity(c.TotalSales.Value() + amount)
+	if err != nil {
+		return err
+	}
+
+	c.TotalSales = newTotalSales
+	return nil
+}
+
+func (c *Campaign) Close() error {
+	closeStatus, err := valueobject.NewStatus(valueobject.Ended)
+	if err != nil {
+		return err
+	}
+
+	c.Status = closeStatus
+	return nil
+}
+
+func (c *Campaign) IsActive() bool {
+	return c.Status.Value() == valueobject.Active
+}
+
+func (c *Campaign) DecreaseDuration(duration int) error {
+	newDuration, err := valueobject.NewDuration(c.Duration.Value() - duration)
+	if err != nil {
+		return err
+	}
+
+	c.Duration = newDuration
+	return nil
+}
+
+func (c *Campaign) RemainingTargetSalesCount(quantity int) int {
+	return c.TargetSalesCount.Value() - (quantity + c.TotalSales.Value())
+}
+
+func (c *Campaign) UpdateAverageItemPrice(orderPrice float64, orderQuantity int) error {
+	if c.TotalSales.Value() == 0 {
+		return nil
+	}
+
+	orderRevenue := orderPrice * float64(orderQuantity)
+	oldTotalRevenue := c.AverageItemPrice.Value() * float64(c.TotalSales.Value()-orderQuantity)
+
+	newTotalRevenue := oldTotalRevenue + orderRevenue
+
+	newAverageItemPrice, err := valueobject.NewPrice(newTotalRevenue / float64(c.TotalSales.Value()))
+	if err != nil {
+		return err
+	}
+	c.AverageItemPrice = newAverageItemPrice
+	return nil
 }
