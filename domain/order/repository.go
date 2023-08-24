b72d9c4dc71d47e96c0d8dc85a8c264ac7a1c4ab
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-08-24 17:47:06 +0300

    feat: add go:generate mockgen

--- domain/order/repository.go
+++ domain/order/repository.go
@@ -10,6 +10,7 @@ var (
 	ErrOrderAlreadyExist = errors.New("Order already exist")
 )
 
+//go:generate mockgen -destination=../../mock/repository/order/order.go -package=repository github.com/aaydin-tr/e-commerce/domain/order OrderRepository
 type OrderRepository interface {
 	Create(order *entity.Order) error
 }
--- domain/product/repository.go
+++ domain/product/repository.go
@@ -12,6 +12,7 @@ var (
 	ErrAlreadyExist = errors.New("Product already exist")
 )
 
+//go:generate mockgen -destination=../../mock/repository/product/product.go -package=repository github.com/aaydin-tr/e-commerce/domain/product ProductRepository
 type ProductRepository interface {
 	Get(code valueobject.Code) (*entity.Product, error)
 	Create(product *entity.Product) error
